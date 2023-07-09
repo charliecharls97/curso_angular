00f071e1f2edfd2d8b5d082284c92f6bbd78acc6
charlie <carlosibgroup@gmail.com>
2023-07-09 12:09:29 -0500

    curso-angular-parte-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Configuracion } from './models/configuracion';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Curso de Angular';
+export class AppComponent{
+    public title: string;
+    public descripcion: string;
+    public textButton: string;
+    public mostrar_videojuego: boolean;
+    public config;
+   
+
+    constructor(){
+        this.config =  Configuracion;
+
+        this.title = this.config.title;
+        this.descripcion = this.config.descripcion;
+        
+        this.mostrar_videojuego = true;
+        this.textButton = "Ocultar videojuego";
+        
+    }
+
+    ocultarVideojuego(){
+        this.mostrar_videojuego = !this.mostrar_videojuego;
+        if(this.mostrar_videojuego){
+            this.textButton = "Ocultar videojuego";
+        }else{
+            this.textButton = "Mostrar videojuego";
+        }
+    }
+  
 }
